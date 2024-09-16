780c17eaef77aa214e5d0dd1875a61d2d74e2b54
Ivan Patskan <hosp54@DESKTOP-1CFD7GK.>
2024-09-16 15:07:39 +0700

    Add addition function

--- HW2_Patskan/calculator.py
+++ HW2_Patskan/calculator.py
@@ -1,3 +1,7 @@
 # Description - program calculates basic mathematical operations: +,-,*,/
 # Input - user enters numbers and operation sign
 # Authors: Афанасьева, Кудашева, Пацкан, Шипунова
+
+#Функция сложения
+def addition(a, b):
+    return a + b
