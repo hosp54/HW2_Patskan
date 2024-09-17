5bae701b4e0f59dc34b4437783f63601e22a356c
ItValeria <aopex@mail.ru>
2024-09-17 23:59:39 +0300

    add subtraction

--- HW2_Patskan/calculator.py
+++ HW2_Patskan/calculator.py
@@ -2,6 +2,6 @@
 # Input - user enters numbers and operation sign
 # Authors: Афанасьева, Кудашева, Пацкан, Шипунова
 
-#Функция сложения
-def addition(a, b):
-    return a + b
+#Функция вычитания
+def subtraction(a, b):
+    return a - b
